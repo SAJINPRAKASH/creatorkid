e68196cc1c4a96c9ee1299b43941d70943c6ee13
Sajin Prakash <sajinprakash989@gmail.com>
2026-05-03 23:27:25 +0530

    [DELETE] CK001 - 2026-05-03 23:27:25

--- js/data.js
+++ js/data.js
@@ -1,14 +1,6 @@
 // Central Data Store for CreatorKid
 
 const prompts = [
-    {
-        id: "CK001",
-        title: "car",
-        category: "ImageGen",
-        description: "asdasdadsasdas",
-        promptText: "sdasdasdasdas",
-        image: "assets/images/first slide 3.jpg"
-    }
 ];
 
 const resources = [
