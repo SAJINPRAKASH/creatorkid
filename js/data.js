34863ec7661040747bddef785d458ea38a6fe7ec
Sajin Prakash <sajinprakash989@gmail.com>
2026-05-03 23:10:33 +0530

    [DELETE] RS001 - 2026-05-03 23:10:33

--- js/data.js
+++ js/data.js
@@ -4,15 +4,6 @@ const prompts = [
 ];
 
 const resources = [
-    {
-        id: "RS001",
-        title: "gdsgsd",
-        category: "Presets",
-        description: "sdfgsdfgdg",
-        main_file: "assets/downloads/Anu_s_pdf.pdf",
-        image: "assets/images/12 x 18 print.jpg",
-        downloads: "dfgsd"
-    }
 ];
 
 const aiTools = [
